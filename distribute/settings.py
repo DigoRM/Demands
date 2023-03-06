6ffab5569058179ae284cf2473a93aefaca6ee8e
Rodrigo Rodrigues Marcolino <82246925+DigoRM@users.noreply.github.com>
2023-03-05 22:08:45 -0300

    Update settings.py

--- distribute/settings.py
+++ distribute/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6**cj17gslh6xv&t19a+skw!#@48(!v+vr(nchhul6%1k_u&m3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
